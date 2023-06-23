72f3882b323b4f1eb06dd51838da8d212ae3fbf3
zbluee <amanueltemesgen9@gmail.com>
2023-06-23 04:16:30 -0500

    feat: Add routing and page components to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import HomePage from './pages/home/home-page';
+import DonationPage from './pages//donation/donation-page';
+import DonationHistoryPage from './pages/donation-history/donation-history';
+import VolunteerOpportunitiesPage from './pages/volunteer-opportunities-page/volunteer-opportunities-page';
+import AboutUsPage from './pages/about-us-page/about-us-page';
+import FAQPage from './pages/faq-page/faq-page';
+import ContactUsPage from './pages/contact-us-page/contact-us-page';
+import NotFoundPage from './pages/not-found/not-found';
+import Footer from './components/footer/footer';
+import Header from './components/header/header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container" >
+    <Header/>
+    <div className="main-content ">
+    <Routes>
+        <Route path="/" element={<HomePage/>} />
+        <Route path="/donate" element={<DonationPage/>} />
+        <Route path="/donation-history" element={<DonationHistoryPage/>} />
+        <Route path="/volunteer-opportunities" element={<VolunteerOpportunitiesPage/>} />
+        <Route path="/about-us" element={<AboutUsPage/>} />
+        <Route path="/faq" element={<FAQPage/>} />
+      <Route path="/contact-us" element={<ContactUsPage/>} /> 
+      <Route path="*" element={<NotFoundPage/>} /> 
+    </Routes>
+    </div>
+    <Footer/>
     </div>
   );
 }
