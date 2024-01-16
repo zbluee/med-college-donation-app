175eb4d66fdfc739de553d49405d6af4c24b38be
zbluee <amanueltemesgen9@gmail.com>
2024-01-16 02:39:47 -0500

    feat: Add content and style to about-page.

--- client/src/pages/about-us-page/about-us-page.jsx
+++ client/src/pages/about-us-page/about-us-page.jsx
@@ -8,16 +8,28 @@ function AboutUsPage() {
         <div className="about-us-description">
           <h1 className="about-us-title">About Us</h1>
           <p className="about-us-text">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur pretium lacus ac nibh
-            malesuada, sed mattis felis tristique. Aliquam erat volutpat. Pellentesque habitant
-            morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nullam consequat,
-            tellus ac aliquam hendrerit, elit massa consequat mi, ut faucibus mi ligula id purus.
+          Welcome to the Med-College Donation App, your gateway to supporting the future of medicine! We are delighted to introduce you to a platform that facilitates seamless online donations, provides information on volunteer opportunities, offers an FAQ section, and ensures easy access to contact information.
+
+At the heart of this initiative is our commitment to advancing medical education, research, and healthcare at Tibebe Ghion Specialized Teaching Hospital. The Med-College Donation App streamlines the process of contributing to the Medicine College, making it convenient for individuals and organizations to make a positive impact on the medical community.
           </p>
           <p className="about-us-text">
-            Vestibulum congue neque sed ligula finibus, ac aliquet mauris venenatis. Nunc
-            egestas, urna sit amet efficitur consectetur, nunc odio congue enim, et fringilla enim
-            diam ut dui. Donec faucibus, elit a porttitor dictum, ante tortor imperdiet sem, ac
-            dapibus felis lacus a erat. Sed in tortor at erat volutpat vestibulum.
+          <h3>Our Mission:</h3>
+
+The Med-College Donation App is more than just a platform; it's a commitment to the advancement of medical education and healthcare. By fostering a community of support, we aim to contribute to the growth and excellence of Medicine College.
+
+<h3>Meet the Creator:</h3>
+
+
+This initiative was brought to life by <a
+            href="https://t.me/Danniielo"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="about-link"
+          >
+           <strong>Daniel Esubalew Dessie</strong>
+          </a>, a visionary creator passionate about leveraging technology to make a positive impact. Daniel envisions a future where education and healthcare go hand in hand, and the Med-College Donation App is a testament to his dedication to this cause.
+
+<div className="about-foot"> Thank you for being part of our journey. Together, let's shape the future of medicine!</div> 
           </p>
         </div>
       </div>
